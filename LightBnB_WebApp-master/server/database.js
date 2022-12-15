37eba6e5432635865835c4afa6481d645cbe0206
Jesse Giles <jesse.giles@telus.com>
2022-12-15 16:35:05 -0700

    completed lighthouseBnB add property refactoring assignment, tested code working

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -62,10 +62,6 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser =  function(user) {
 
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-
   const insertNewUser = `INSERT INTO users (
     name, email, password) 
     VALUES ($1, $3, $2) RETURNING *;`;
@@ -200,9 +196,18 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const insertNewProperty = `INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code
+  ) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`;
+    const values = [property.title, property.description, property.owner_id, property.cover_photo_url, property.thumbnail_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.province, property.city, property.country, property.street, property.post_code]
+
+  return pool.query(insertNewProperty, values)
+    .then((property) => {
+      return (property.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
 }
 exports.addProperty = addProperty;
